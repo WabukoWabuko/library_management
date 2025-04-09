dfac2add272b1b4434819a18a3c8b2eb9e5f9d56
WabukoWabuko <basilwabbs@gmail.com>
2025-04-09 19:20:01 +0300

    Fixed test_return_book to simulate issuing before returning

--- library_app/tests.py
+++ library_app/tests.py
@@ -30,16 +30,29 @@ class LibraryAPITestCase(TestCase):
         self.assertEqual(Transaction.objects.count(), 1)
 
     def test_return_book(self):
-        # Issue a book first
-        transaction = Transaction.objects.create(book=self.book, member=self.member)
+        # Issue the book first to decrease stock
+        self.client.post('/api/issue/', {
+            'book_id': self.book.id,
+            'member_id': self.member.id
+        }, content_type='application/json')
+        
+        # Check stock decreased
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.stock, 4)  # Stock should be 5 - 1 = 4
+        
+        # Get the transaction (the one we just created)
+        transaction = Transaction.objects.get(book=self.book, member=self.member, return_date__isnull=True)
+        
+        # Return the book
         response = self.client.post('/api/return/', {
             'transaction_id': transaction.id
         }, content_type='application/json')
+        
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.book.refresh_from_db()
         self.member.refresh_from_db()
-        self.assertEqual(self.book.stock, 5)
-        self.assertGreater(self.member.debt, 0)
+        self.assertEqual(self.book.stock, 5)  # Stock should be back to 5
+        self.assertGreater(self.member.debt, 0)  # Debt should increase
 
     def test_search_books(self):
         response = self.client.get('/api/search/?q=Test')
