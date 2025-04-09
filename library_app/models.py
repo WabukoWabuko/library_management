97015fff9a108836fa49fd78b267ef96f1bc604a
WabukoWabuko <basilwabbs@gmail.com>
2025-04-09 17:17:53 +0300

    Added models and admin setup for Book, Member, Transaction

--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} by {self.author}"
+
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    debt = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    issue_date = models.DateTimeField(default=timezone.now)
+    return_date = models.DateTimeField(null=True, blank=True)
+    fee = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f"{self.book.name} issued to {self.member.name}"
